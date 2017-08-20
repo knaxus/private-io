bb46e124ad1fb939f7cd014af3e6edca849bbb87
Ashok Dey <ashokdey100@gmail.com>
2017-08-20 20:30:25 +0530

    added React and material-ui

---
+++ client/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+ 
+// Needed for onTouchTap 
+// http://stackoverflow.com/a/34015469/988941 
+injectTapEventPlugin();
