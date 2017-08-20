f6e55263f44ea5ee39c8212e95dc44dad5630ee0
Ashok Dey <ashokdey100@gmail.com>
2017-08-20 20:25:04 +0530

    setup socket-io

--- server/app.js
+++ server/app.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
+import http from 'http';
+import socketIO from 'socket.io';
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 /**
  * use the public path for static files
  */
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
-  console.log('app running at:  http://localhost:' + port);
+server.listen(port, () => {
+  console.log('running at : http://localhost:' + port);
 });
